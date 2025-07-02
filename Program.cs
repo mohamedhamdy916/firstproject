27538a7cfff9085be594f5ffac068bdca76088dc
Mohamed pc <Mohamed pc@DESKTOP-KPV03GE>
2025-07-03 02:39:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace firstproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("enter your favorite number betwen 1 and 100");
+            int Number = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine($"no really!! ,{ Number} is my fav number");
+            
+
+        }
+    }
+}
