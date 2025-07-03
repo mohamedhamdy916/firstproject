91b3132945344e4c65ad36c91bd1e40bfcd2b7fd
mohamedhamdy916 <mh0101911@gmail.com>
2025-07-03 03:53:38 +0300

    Revert "Update Program.cs"

--- Program.cs
+++ Program.cs
@@ -1,25 +1,15 @@
-namespace firstproject
+﻿namespace firstproject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("enter number of small carpet :");
-            int smallcarpet = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter number of large carpet :");
-            int largecarpet = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(" price per small room : $25");
-            Console.WriteLine(" price per large room : $35");
-            Console.WriteLine($"cost : {smallcarpet * 25 + largecarpet * 35}");
-            int cost = Convert.ToInt32(smallcarpet * 25 + largecarpet * 35);
-            double Tax = Convert.ToDouble(.06 * cost);
-            Console.WriteLine($"Tax = $ {Tax}");
-            Console.WriteLine("-------------------------------");
-            Console.WriteLine($"Total estimate : {cost + Tax}");
-            Console.WriteLine("This estimate is valid for 30 days");
+           
+            Console.WriteLine("enter your favorite number betwen 1 and 100");
+            int Number = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine($"no really!! ,{ Number} is my fav number");
+            
 
         }
-        
     }
 }
